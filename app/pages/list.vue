54828ce03164a091429d4114cd68e29adec9c340
broadsign-lekevoid <kevin.gagnon@broadsign.com>
2025-09-28 15:34:56 -0400

    Install Content, Hub

---
+++ app/pages/list.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<h1>Hello world again</h1>
+		<div>
+			<pre>
+		{{ page }}
+		</pre
+			>
+		</div>
+	</div>
+</template>
+
+<script setup>
+const route = useRoute();
+const { data: page } = await useAsyncData(route.path, () => {
+	return queryCollection("docs").path(route.path).first();
+});
+</script>
+
+<style lang="scss"></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+	compatibilityDate: "2025-07-15",
+	devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/ui'
-  ]
-})
+	modules: ["@nuxt/content", "@nuxt/eslint", "@nuxt/image", "@nuxt/ui", "@nuxthub/core"],
+
+	content: {
+		build: {
+			markdown: {
+				toc: {
+					depth: 3, // include h3 headings
+				},
+			},
+		},
+	},
+
+	hub: {
+		// NuxtHub options
+	},
+});
